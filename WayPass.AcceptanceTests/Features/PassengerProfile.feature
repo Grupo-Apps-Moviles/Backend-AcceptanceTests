b5c73716e8a1efbf6a595e1c1654812de7274504
Yaku Guzman <yakumgc12@gmail.com>
2026-06-09 21:02:49 -0500

    test: add BDD acceptance scenarios for US29 passenger profile
    Covers profile view and update endpoints (GET /api/v1/users/{userId}/profile,
    PUT /api/v1/profiles/{id}) for US29.

---
+++ WayPass.AcceptanceTests/Features/PassengerProfile.feature
@@ -0,0 +1,25 @@
+﻿Feature: Passenger Profile Management
+  As an authenticated passenger
+  I want to view and update my profile
+  So that my personal information is always up to date
+
+  Background:
+    Given the WayPass backend is running
+    And I am an authenticated Passenger
+
+  # US29 - Editar perfil de usuario
+  Scenario: Retrieve my profile by user id
+    When I request the profile associated to my user id
+    Then the response status should be 200
+    And the response should include my profile information
+
+  Scenario: Update my profile with valid data
+    Given I have a valid profile update payload
+    When I send a profile update request
+    Then the response status should be 200
+    And the profile should reflect the updated information
+
+  Scenario: Update profile fails with invalid data
+    Given I have an invalid profile update payload
+    When I send a profile update request
+    Then the response status should be 400
---
+++ WayPass.AcceptanceTests/Steps/PassengerProfileSteps.cs
@@ -0,0 +1,34 @@
+﻿using Reqnroll;
+using WayPass.AcceptanceTests.Support;
+
+namespace WayPass.AcceptanceTests.Steps;
+
+[Binding]
+public class PassengerProfileSteps
+{
+    private readonly Support.ScenarioContext _ctx;
+    public PassengerProfileSteps(Support.ScenarioContext ctx) => _ctx = ctx;
+
+    // Pasos reutilizados desde PassengerAuthenticationSteps:
+    //   Given the WayPass backend is running
+    //   Given I am an authenticated Passenger
+    //   Then the response status should be {int}
+
+    [Given(@"I have a valid profile update payload")]
+    public void GivenValidProfilePayload() { /* TODO(impl) */ }
+
+    [Given(@"I have an invalid profile update payload")]
+    public void GivenInvalidProfilePayload() { /* TODO(impl) */ }
+
+    [When(@"I request the profile associated to my user id")]
+    public void WhenRequestProfile() { /* TODO(impl) */ }
+
+    [When(@"I send a profile update request")]
+    public void WhenSendProfileUpdate() { /* TODO(impl) */ }
+
+    [Then(@"the response should include my profile information")]
+    public void ThenResponseIncludesProfileInfo() { /* TODO(impl) */ }
+
+    [Then(@"the profile should reflect the updated information")]
+    public void ThenProfileReflectsUpdate() { /* TODO(impl) */ }
+}
